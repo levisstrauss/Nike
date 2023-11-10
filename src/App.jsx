98474a0fab22cee9c522105fddf9c1aa2dc5c747
Zakaria Coulibaly <kategnimincoulibaly@MacBook-Pro.local>
2023-11-10 10:55:10 -0500

    Project set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => (
+    <main className="relative">
+        nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+
+        </section>
+        <section className="padding">
+
+        </section>
+    </main>
+)
+export default App;
